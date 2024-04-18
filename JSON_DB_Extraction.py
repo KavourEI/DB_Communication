2dbe27d5fe2aa374d4a218502b50e2c67026978b
Themis Kavour <69631669+KavourEI@users.noreply.github.com>
2024-04-18 22:42:21 +0300

    Added comments to JSON_DB_Extraction

--- JSON_DB_Extraction.py
+++ JSON_DB_Extraction.py
@@ -1,21 +1,18 @@
 import sqlite3
 import json
 
-df_file = 'local_database.db'
+df_file = 'local_database.db'    # The DB file created from earlier, containing all the precious data of yours! 
 
-# Connect to the SQLite database
 conn = sqlite3.connect(df_file)
 cursor = conn.cursor()
 
-# Get the table names in the database
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")    # Get all the table names in the database
 tables = cursor.fetchall()
 tables
-# Dictionary to store table information
-tables_info = {}
 
-# Iterate over tables
-for feature in tables:
+tables_info = {}    # Hmmm mind that here! We are going to fill this object with the included features.
+
+for feature in tables:    # Iterate to collect feature names, yes at this point just feature names! 
     table_name = feature[0]
     cursor.execute("PRAGMA table_info('{}')".format(table_name))
     columns = cursor.fetchall()
@@ -26,11 +23,9 @@ for feature in tables:
         columns_info.append({'name': column_name, 'type': column_type})
     tables_info[table_name] = columns_info
 
-# Close the connection
 conn.close()
 
-# Write table information to a JSON file
 with open(f'{df_file.split('.')[0]}.json', 'w') as json_file:
-    json.dump(tables_info, json_file, indent=4)
+    json.dump(tables_info, json_file, indent=4)    # Export information to a json file.
 
-print("Table information has been exported to database_info.json")
+print(f"Table information has been exported to {df_file.split('.')[0]}.json")
